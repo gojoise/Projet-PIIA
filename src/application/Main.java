d7a9c93d8ba07dbf18cf13e0088c59975c926180
gojoise <mathieu.benard@u-psud.fr>
2021-04-27 15:42:58 +0200

    window

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,16 +6,66 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class Main extends Application {
+
+	//Nous créons des variable de classes afin de pouvoir y accéder partout
+	//Ceci afin de pouvoir y positionner les éléments que nous avons fait
+	//Il y a un BorderPane car le conteneur principal de notre IHM
+	//est un BorderPane, nous reparlerons de l'objet Stage
+	private Stage stagePrincipal;
+	private BorderPane conteneurPrincipal;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		stagePrincipal = primaryStage;
+		//Ca ne vous rappelle pas une JFrame ?
+		stagePrincipal.setTitle("titre");
+		
+		//Nous allons utiliser nos fichier FXML dans ces deux méthodes
+		initialisationConteneurPrincipal();
+		initialisationContenu();
+	}
+
+	private void initialisationConteneurPrincipal() {
+		//On créé un chargeur de FXML
+		FXMLLoader loader = new FXMLLoader();
+		//On lui spécifie le chemin relatif à notre classe
+		//du fichier FXML a charger : dans le sous-dossier view
+		loader.setLocation(Main.class.getResource("MainWindow.fxml"));
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			//Le chargement nous donne notre conteneur
+			conteneurPrincipal = (BorderPane) loader.load();
+			//On définit une scène principale avec notre conteneur
+			Scene scene = new Scene(conteneurPrincipal);
+			//Que nous affectons à notre Stage
+			stagePrincipal.setScene(scene);
+			//Pour l'afficher
+			stagePrincipal.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void initialisationContenu() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("MainWindow.fxml"));
+		try {
+			//Nous récupérons notre conteneur qui contiendra les données
+			//Pour rappel, c'est un AnchorPane...
+			AnchorPane conteneurPersonne = (AnchorPane) loader.load();
+			//Qui nous ajoutons à notre conteneur principal
+			//Au centre, puisque'il s'agit d'un BorderPane
+			conteneurPrincipal.setCenter(conteneurPersonne);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
